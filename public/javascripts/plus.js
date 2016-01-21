879856dd23c4ab5702c6e3c68cb351a97ebe3f7b
Cristhian López <cplopez4@uc.cl>
2016-01-21 00:44:20 -0300

    cambios R v1

--- public/javascripts/plus.js
+++ public/javascripts/plus.js
@@ -5,22 +5,18 @@ $(document).ready(function(){
 		$(value).qtip({
             style: {
                 //classes: area,
-                tip: {
-                    width: 8,
-                    height: 12
-                }
+                tip: false
             },
             content: {
-                text: "<div><p class='tooltip-title'>"+ title +"</p></div>"
+                text: "<div class='tooltip-top'><p class='tooltip-title'>"+ title +"</p></div><div class='tooltip-down'><div class='tooltip-down-l'><p class='tooltip-media'>"+ title +"</p></div><div class='tooltip-down-r'><p class='tooltip-mediatype'>"+ title +"</p></div></div>"
             },
             position: {
                 my: 'bottom left',
                 at: 'top left',
-				target: 'mouse',
+				/*target: 'mouse',*/
                 adjust: {
-                	mouse: false,
-                	x: 5,
-                	y: -7
+                	x: 0,
+                	y: 200
                 }
             },
             show: {
@@ -43,6 +39,11 @@ $(document).ready(function(){
 	    $(".tl-timemarker").find(".tl-timemarker-content-container").removeClass("full-opacity");
 	  }
 	);
+
+	$(document).on('click',".tl-timemarker",function(){
+		var id = parseInt($(this).attr("data-hasqtip")) + 1;
+		history.pushState(null, null, "?news=" + id);
+	});
 
 	$('#filter-dropdown').on('change', function (e) {
 	    var valueSelected = this.value;
@@ -99,3 +100,17 @@ $(document).ready(function(){
 	    })
 	});
 });
+
+(function($) {
+    $.QueryString = (function(a) {
+        if (a == "") return {};
+        var b = {};
+        for (var i = 0; i < a.length; ++i)
+        {
+            var p=a[i].split('=');
+            if (p.length != 2) continue;
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+        }
+        return b;
+    })(window.location.search.substr(1).split('&'))
+})(jQuery);
